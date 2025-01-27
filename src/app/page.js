f5c4a558ca40b6caa01ea182cf798422d0548c15
Vskroi <24LP1862@G301-CC0201.local>
2025-01-27 15:07:55 +0800

    a

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,25 @@ import { StepTwo } from "@/components/StepTwo";
 import { StepThree } from "@/components/three";
 import { StepFour } from "@/components/StepFour";
 export default function Home() {
-  const [step, setStep] = useState(1
-);
-  useEffect(() => {
-    if(typeof window !== 'undefined'){
-    const storedData = localStorage.getItem("page");
-      const parsedData =JSON.parse(storedData)   ;
-      setStep(parsedData);}
-  },[], )
-useEffect(()=> {
+  const [step, setStep] = useState(1);
+  
   if(typeof window !== 'undefined'){
-  const jsonData = JSON.stringify(step);
-  localStorage.setItem("page", jsonData);}
-},[step])
 
+useEffect(() => {
+  const storedData = localStorage.getItem("page");
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    setStep(parsedData);
+  }
+}, []);  
 
+useEffect(() => {
+  if (step !== 1) {  
+    const jsonData = JSON.stringify(step);
+    localStorage.setItem("page", jsonData);
+  }
+}, [step]);
+  }
   return (
     <div className="w-screen h-screen flex-col justify-center items-center inline-flex">
       {step === 1 && <StepOne setStep={setStep} />}
